c35c882b2218e0a293ae79afe46db0a1b22159d7
yoseiAndplus <yosei@andplus.co.jp>
2024-01-06 18:15:35 +0900

    配列を逆順に

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,19 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  const postsByMonth = data.allMarkdownRemark.edges.reduce(
-    (posts, { node }) => {
-      const date = node.frontmatter.date
-      let parts = date.split(" ")
-      let year = parts[0]
-      let month = parts[1]
+  const postsByYear = data.allMarkdownRemark.edges.reduce((posts, { node }) => {
+    const date = node.frontmatter.date
+    let parts = date.split(" ")
+    let year = parseInt(parts[0])
 
-      if (!posts[year]) {
-        posts[year] = {}
-      }
-      if (!posts[year][month]) {
-        posts[year][month] = []
-      }
-      posts[year][month].push(node)
-      return posts
-    },
-    {}
-  )
+    if (!posts[year]) {
+      posts[year] = []
+    }
+    posts[year].push(node)
+    return posts
+  }, {})
 
-  console.log(postsByMonth)
+  console.log(postsByYear)
 
   if (posts.length === 0) {
     return (
@@ -44,66 +37,67 @@ const BlogIndex = ({ data, location }) => {
       <div class={styles.postsGrid}>
         <h2>global</h2>
         <ol style={{ listStyle: `none` }}>
-          {Object.keys(postsByMonth).map(year =>
-            Object.keys(postsByMonth[year]).map(month =>
-              postsByMonth[year][month].map(post => {
-                const title = post.frontmatter.title || post.fields.slug
-                return post.frontmatter.category === "global" && post.frontmatter.status === "public" ? (
-                  <li key={post.fields.slug}>
-                    <article
-                      className="post-list-item"
-                      itemScope
-                      itemType="http://schema.org/Article"
-                    >
-                      <header>
-                        <h3>
-                          <Link to={post.fields.slug} itemProp="url">
-                            <span itemProp="headline">{title}</span>
-                          </Link>
-                        </h3>
-                      </header>
-                    </article>
-                  </li>
-                ) : null
-              })
-            )
+          {Object.keys(postsByYear).reverse().map(year =>
+            postsByYear[year].map(post => {
+              const title = post.frontmatter.title || post.fields.slug
+              return post.frontmatter.category === "global" &&
+                post.frontmatter.status === "public" ? (
+                <li key={post.fields.slug}>
+                  <article
+                    className="post-list-item"
+                    itemScope
+                    itemType="http://schema.org/Article"
+                  >
+                    <header>
+                      <h3>
+                        <Link to={post.fields.slug} itemProp="url">
+                          <span itemProp="headline">{title}</span>
+                        </Link>
+                      </h3>
+                    </header>
+                  </article>
+                </li>
+              ) : null
+            })
           )}
         </ol>
       </div>
       <div class={styles.postsGrid}>
-        {Object.keys(postsByMonth).map(year => {
+        {Object.keys(postsByYear).reverse().map(year => {
           return (
             <>
               <h2 class={styles.label}>{year}</h2>
               <ol style={{ listStyle: `none` }}>
-                {Object.keys(postsByMonth[year]).map(month =>
-                  postsByMonth[year][month].map(post => {
-                    const title = post.frontmatter.title || post.fields.slug
-                    const date = post.frontmatter.date
-                    let parts = date.split(" ")
-                    let month = parts[1]
-                    let day = parts[2]
-                    return post.frontmatter.category === "post" && post.frontmatter.status === "public" ? (
-                      <li key={post.fields.slug}>
-                        <article
-                          className="post-list-item"
-                          itemScope
-                          itemType="http://schema.org/Article"
-                        >
-                          <header>
-                            <h3>
-                              <Link to={post.fields.slug} itemProp="url">
-                                <div class={styles.title} itemProp="headline">
-                                  <span class={styles.day}>{month}-{day}</span><span>{title}</span>
-                                </div>
-                              </Link>
-                            </h3>
-                          </header>
-                        </article>
-                      </li>
-                    ) : null
-                  })
-                )}
+                {postsByYear[year].map(post => {
+                  const title = post.frontmatter.title || post.fields.slug
+                  const date = post.frontmatter.date
+                  let parts = date.split(" ")
+                  let month = parts[1]
+                  let day = parts[2]
+                  return post.frontmatter.category === "post" &&
+                    post.frontmatter.status === "public" ? (
+                    <li key={post.fields.slug}>
+                      <article
+                        className="post-list-item"
+                        itemScope
+                        itemType="http://schema.org/Article"
+                      >
+                        <header>
+                          <h3>
+                            <Link to={post.fields.slug} itemProp="url">
+                              <div class={styles.title} itemProp="headline">
+                                <span class={styles.day}>
+                                  {month}-{day}
+                                </span>
+                                <span>{title}</span>
+                              </div>
+                            </Link>
+                          </h3>
+                        </header>
+                      </article>
+                    </li>
+                  ) : null
+                })}
               </ol>
             </>
           )
