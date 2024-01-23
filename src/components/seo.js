9a5b4d6e7eb3c56e89d097e29fc4916238cda990
yosei <yoppikozouozokippoy@gmail.com>
2024-01-23 23:59:23 +0900

    seoの修正

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,41 +7,72 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { useLocation } from "@reach/router"
 
 const Seo = ({ description, title, children }) => {
-  const { site } = useStaticQuery(
+  const { site, file } = useStaticQuery(
     graphql`
       query {
         site {
           siteMetadata {
             title
             description
-            social {
-              twitter
-            }
+            siteUrl
+            ogpImg
+            ogpImgW
+            ogpImgH
+            lang
+            locale
           }
+        }
+        file(name: { eq: "ogp" }) {
+          publicURL
         }
       }
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
+  const defaultLang = site.siteMetadata.lang
+  const lang = defaultLang || `ja`
+
+  const location = useLocation()
+  const pagePath = location.pathname ? location.pathname : ""
   const defaultTitle = site.siteMetadata?.title
+  const defaultDescription = site.siteMetadata?.description
+  const metaTitle = defaultTitle ? `${title} | ${defaultTitle}` : title
+  const metaDescription = description ? description : defaultDescription
+  const siteUrl = site.siteMetadata?.siteUrl
+  const metaUrl = pagePath ? `${siteUrl}${pagePath}` : `${siteUrl}`
+  const metaType = pagePath ? `website` : `article`
+  const defaultOgp = site.siteMetadata?.ogpImg
+  const ogpImg = file.publicURL
+  const ogpImgW = site.siteMetadata?.ogpImgW
+  const ogpImgH = site.siteMetadata?.ogpImgH
+  const metaOgpImg = ogpImg ? `${siteUrl}${ogpImg}` : `${siteUrl}${defaultOgp}`
+  const metaOgpImgW = ogpImgW || `1200`
+  const metaOgpImgH = ogpImgH || `630`
+  const defaultLocale = site.siteMetadata.locale
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <html lang={lang} />
+      <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:site_name" content={defaultTitle} />
       <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:creator"
-        content={site.siteMetadata?.social?.twitter || ``}
-      />
-      <meta name="twitter:title" content={title} />
+      <meta property="og:type" content={metaType} />
+      <meta property="og:url" content={metaUrl} />
+      <meta property="og:image" content={metaOgpImg} />
+      <meta property="og:image:width" content={metaOgpImgW} />
+      <meta property="og:image:height" content={metaOgpImgH} />
+      <meta property="og:locale" content={defaultLocale} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
+
+      <link rel="icon" href="/image/favicon.ico" />
+
       {children}
     </>
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,19 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+
+
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="blog-post">
+        <header>
+          <h1>404: Not Found</h1>
+        </header>
+        <p>ここは静かで心地がいいですよ。</p>
+      </div>
     </Layout>
   )
 }
