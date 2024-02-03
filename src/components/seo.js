ced981e4b5ee53effde8503d905ecf59ba528d97
yosei <yoppikozouozokippoy@gmail.com>
2024-02-03 22:20:20 +0900

    headの修正

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,81 +1,107 @@
-/**
- * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
-import * as React from "react"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { useLocation } from "@reach/router"
 
-const Seo = ({ description, title, children }) => {
-  const { site, file } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            siteUrl
-            ogpImg
-            ogpImgW
-            ogpImgH
-            lang
-            locale
-          }
-        }
-        file(name: { eq: "ogp" }) {
-          publicURL
+export const Seo = props => {
+  const {
+    pageTitle,
+    pageExcerpt,
+    pagePath,
+    pageType,
+    pageImage,
+    pageImageW,
+    pageImageH,
+  } = props
+  const {
+    wp: {
+      generalSettings: { title, description },
+    },
+    site: {
+      siteMetadata: {
+        defaultTitle,
+        defaultDescription,
+        defaultSiteUrl,
+        facebookID,
+        twitter,
+        defaultLang,
+        defaultLocale,
+        defaultOgpImg,
+        defaultOgpImgW,
+        defaultOgpImgH,
+      },
+    },
+    file: { publicURL },
+  } = useStaticQuery(graphql`
+    query SeoQuery {
+      site {
+        siteMetadata {
+          defaultTitle
+          defaultDescription
+          defaultSiteUrl
+          twitter
+          defaultLang
+          defaultLocale
+          defaultOgpImg
+          defaultOgpImgW
+          defaultOgpImgH
         }
       }
-    `
-  )
-
-  const defaultLang = site.siteMetadata.lang
+      file(name: { eq: "ogp" }) {
+        publicURL
+      }
+    }
+  `)
+  /*-- langの設定 --*/
   const lang = defaultLang || `ja`
-
-  const location = useLocation()
-  const pagePath = location.pathname ? location.pathname : ""
-  const defaultTitle = site.siteMetadata?.title
-  const defaultDescription = site.siteMetadata?.description
-  const metaTitle = defaultTitle ? `${title} | ${defaultTitle}` : title
-  const metaDescription = description ? description : defaultDescription
-  const siteUrl = site.siteMetadata?.siteUrl
+  /*-- タイトルの設定 --*/
+  const siteName = title || defaultTitle
+  const metaTitle = pageTitle ? pageTitle + "｜" + siteName : siteName
+  /*-- 説明文の設定 --*/
+  const siteDescription = description ? description : defaultDescription
+  const metaDescription = pageExcerpt
+    ? pageExcerpt.length > 120
+      ? pageExcerpt.slice(0, 120) + "..."
+      : pageExcerpt
+    : siteDescription
+  /*-- ページURLの設定 --*/
+  const siteUrl = defaultSiteUrl
   const metaUrl = pagePath ? `${siteUrl}${pagePath}` : `${siteUrl}`
-  const metaType = !pagePath ? `website` : `article`
-  const defaultOgp = site.siteMetadata?.ogpImg
-  const ogpImg = file.publicURL
-  const ogpImgW = site.siteMetadata?.ogpImgW
-  const ogpImgH = site.siteMetadata?.ogpImgH
-  const metaOgpImg = ogpImg ? `${siteUrl}${ogpImg}` : `${siteUrl}${defaultOgp}`
-  const metaOgpImgW = ogpImgW || `1200`
-  const metaOgpImgH = ogpImgH || `630`
-  const defaultLocale = site.siteMetadata.locale
+  /*-- サイトのtypeの設定 --*/
+  const metaType = pageType ? pageType : `article`
+  /*-- OGP画像の設定 --*/
+  let metaOgpImg = publicURL ? `${siteUrl}${publicURL}` : null
+  let metaOgpImgW = 1200
+  let metaOgpImgH = 630
+
+  if (pageImage) {
+    metaOgpImg = `${siteUrl}${pageImage}`
+    if (pageImageW && pageImageH) {
+      metaOgpImgW = pageImageW
+      metaOgpImgH = pageImageH
+    }
+  }
 
   return (
     <>
-      <html lang={lang} />
+      <html lang="ja" />
+      {/* ページのタイトルや説明文、canical */}
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={metaTitle} />
-      <meta property="og:site_name" content={defaultTitle} />
-      <meta property="og:description" content={metaDescription} />
+      <link rel="canonical" href={metaUrl} />
+      {/* OGP */}
       <meta property="og:type" content={metaType} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
       <meta property="og:url" content={metaUrl} />
       <meta property="og:image" content={metaOgpImg} />
       <meta property="og:image:width" content={metaOgpImgW} />
       <meta property="og:image:height" content={metaOgpImgH} />
+      <meta property="og:site_name" content={siteName} />
       <meta property="og:locale" content={defaultLocale} />
+      <meta property="fb:app_id" content={facebookID} />
+      {/* twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:description" content={metaDescription} />
-
-      <link rel="icon" href="/image/favicon.ico" />
-
-      {children}
+      <meta name="twitter:creator" content={twitter} />
+      <meta name="twitter:site" content={twitter} />
     </>
   )
 }
-
-export default Seo
