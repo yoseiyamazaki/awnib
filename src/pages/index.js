b13f8f1dcb32b56e928b208ff02ae767828a86e6
yosei <yoppikozouozokippoy@gmail.com>
2024-02-04 15:46:54 +0900

    seoとメタ情報の修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -113,13 +113,6 @@ const BlogIndex = ({ data, location }) => {
 
 export default BlogIndex
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="All posts" />
-
 export const pageQuery = graphql`
   {
     site {
@@ -159,3 +152,45 @@ export const pageQuery = graphql`
     }
   }
 `
+
+export const Head = ({ data, location }) => {
+  return (
+    <>
+      <Seo
+        pagePath={location.pathname}
+      />
+      <link rel="stylesheet" href="https://use.typekit.net/zax1sns.css"></link>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;800;900&family=Shippori+Mincho:wght@700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/icon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/icon/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  )
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -30,12 +30,49 @@ const BlogPostTemplate = ({
   )
 }
 
-export const Head = ({ data: { markdownRemark: post } }) => {
+export const Head = ({ data, location }) => {
+  const post = data.markdownRemark
+
   return (
-    <Seo
-      title={post.frontmatter.title}
-      description={post.frontmatter.description || post.excerpt}
-    />
+    <>
+      <Seo
+        pageTitle={post.frontmatter?.title}
+        pageExcerpt={post.frontmatter?.description}
+        pagePath={location.pathname}
+      />
+      <link rel="stylesheet" href="https://use.typekit.net/zax1sns.css"></link>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;800;900&family=Shippori+Mincho:wght@700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/icon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/icon/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
   )
 }
 
